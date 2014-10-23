d3071840381d5b67f9f5451bf8774b8546ea90ac
Matt <matt@builtbyfuse.com>
2014-10-23 23:42:40 +0100

    separating UI testing from core logic

--- src/test.js
+++
@@ -1,5 +0,0 @@
-var test = {
-	function1: function(var1, var2){
-		return var1 + var2;
-	}
-};
--- test/test.js
+++
@@ -1,8 +0,0 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "This has passed the test!" );
-});
-
-QUnit.test( "Test function", function( assert ) {
-	var response = test.function1("Hello ", "world!");
-  	assert.ok(response == "Hello world!", "Function1 operating as expected! It returned - " + response );
-});
